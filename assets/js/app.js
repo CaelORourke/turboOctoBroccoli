6ebf7eab53b05c1b4587342e810aaf823c93e8de
Shane <shanegates@hotmail.com>
2019-05-01 20:59:32 -0700

    call theStrainApi

---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+theStrainApi.data.getStrainDescriptions(1).then(function (response) {console.log(response);});
+theStrainApi.data.getStrainEffects(1).then(function (response) {console.log(response);});
+theStrainApi.data.getStrainFlavors(1).then(function (response) { console.log(response); });
